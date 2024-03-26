ab994712d2df8025c4f66aedcab682dceb43745e
FilmyFather <91774433+FilmyFather@users.noreply.github.com>
2024-03-26 16:35:13 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,12 +2,12 @@ echo "Cloning Repo...."
 if [ -z $BRANCH ]
 then
   echo "Cloning main branch...."
-  git clone https://github.com/FilmyFather/Ultra-Forwardbot JishuDeveloper/Ultra-Forward-Bot
+  git clone https://github.com/FilmyFather/Ultra-Forwardbot FilmyFather/Ultra-ForwardBot
 else
   echo "Cloning $BRANCH branch...."
-  git clone https://github.com/FilmyFather/Ultra-Forwardbot -b $BRANCH /Ultra-Forward-Bot
+  git clone https://github.com/FilmyFather/Ultra-Forwardbot -b $BRANCH /Ultra-ForwardBot
 fi
-cd JishuDeveloper/Ultra-Forward-Bot
+cd FilmyFather/Ultra-ForwardBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
