4a8a91e041a692be70bc253fe8e51ae4034f5ac0
FilmyFather <91774433+FilmyFather@users.noreply.github.com>
2024-03-26 18:00:03 +0530

    Update test.py

--- plugins/test.py
+++ plugins/test.py
@@ -1,12 +1,3 @@
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
-
-
-
-
 import os
 import re 
 import sys
@@ -29,10 +20,8 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 BTN_URL_REGEX = re.compile(r"(\[([^\[]+?)]\[buttonurl:/{0,2}(.+?)(:same)?])")
-BOT_TOKEN_TEXT = "1) Create A Bot Using @BotFather\n\n2) Then You Will Get A Message With Bot Token\n\n3) Forward That Message To Me"
+BOT_TOKEN_TEXT = "<b>1) create a bot using @BotFather\n2) Then you will get a message with bot token\n3) Forward that message to me</b>"
 SESSION_STRING_SIZE = 351
-
-
 
 async def start_clone_bot(FwdBot, data=None):
    await FwdBot.start()
@@ -81,8 +70,6 @@ async def start_clone_bot(FwdBot, data=None):
    FwdBot.iter_messages = iter_messages
    return FwdBot
 
-
-
 class CLIENT: 
   def __init__(self):
      self.api_id = Config.API_ID
@@ -97,25 +84,23 @@ class CLIENT:
         data = data.get('token')
      return Client("BOT", self.api_id, self.api_hash, bot_token=data, in_memory=True)
   
-
-
   async def add_bot(self, bot, message):
      user_id = int(message.from_user.id)
      msg = await bot.ask(chat_id=user_id, text=BOT_TOKEN_TEXT)
      if msg.text=='/cancel':
-        return await msg.reply('Process Cancelled !')
+        return await msg.reply('<b>process cancelled !</b>')
      elif not msg.forward_date:
-       return await msg.reply_text("This Is Not A Forward Message")
+       return await msg.reply_text("<b>This is not a forward message</b>")
      elif str(msg.forward_from.id) != "93372553":
-       return await msg.reply_text("This Message Was Not Forward From Bot Father")
+       return await msg.reply_text("<b>This message was not forward from bot father</b>")
      bot_token = re.findall(r'\d[0-9]{8,10}:[0-9A-Za-z_-]{35}', msg.text, re.IGNORECASE)
      bot_token = bot_token[0] if bot_token else None
      if not bot_token:
-       return await msg.reply_text("There Is No Bot Token In That Message")
+       return await msg.reply_text("<b>There is no bot token in that message</b>")
      try:
        _client = await start_clone_bot(self.client(bot_token, False), True)
      except Exception as e:
-       await msg.reply_text(f"Bot Error :</b> `{e}`")
+       await msg.reply_text(f"<b>BOT ERROR:</b> `{e}`")
      _bot = _client.me
      details = {
        'id': _bot.id,
@@ -128,21 +113,19 @@ class CLIENT:
      await db.add_bot(details)
      return True
     
-
-
   async def add_session(self, bot, message):
      user_id = int(message.from_user.id)
-     text = "<b>⚠️ Disclaimer ⚠️</b>\n\nYou Can Use Your Session For Forward Message From Private Chat To Another Chat.\nPlease Add Your Pyrogram Session With Your Own Risk. Their Is A Chance To Ban Your Account. My Developer Is Not Responsible If Your Account May Get Banned."
+     text = "<b>⚠️ DISCLAIMER ⚠️</b>\n\n<code>you can use your session for forward message from private chat to another chat.\nPlease add your pyrogram session with your own risk. Their is a chance to ban your account. My developer is not responsible if your account may get banned.</code>"
      await bot.send_message(user_id, text=text)
-     msg = await bot.ask(chat_id=user_id, text="<b>Send your pyrogram session.\nget it from @mdsessiongenbot\n\n/cancel - cancel the process</b>")
+     msg = await bot.ask(chat_id=user_id, text="<b>send your pyrogram session.\nget it from @mdsessiongenbot\n\n/cancel - cancel the process</b>")
      if msg.text=='/cancel':
-        return await msg.reply('Process Cancelled !')
+        return await msg.reply('<b>process cancelled !</b>')
      elif len(msg.text) < SESSION_STRING_SIZE:
-        return await msg.reply('Invalid Session String')
+        return await msg.reply('<b>invalid session sring</b>')
      try:
        client = await start_clone_bot(self.client(msg.text, True), True)
      except Exception as e:
-       await msg.reply_text(f"<b>User Bot Error :</b> `{e}`")
+       await msg.reply_text(f"<b>USER BOT ERROR:</b> `{e}`")
      user = client.me
      details = {
        'id': user.id,
@@ -155,26 +138,18 @@ class CLIENT:
      await db.add_bot(details)
      return True
     
-
-
-
-
-
 @Client.on_message(filters.private & filters.command('reset'))
 async def forward_tag(bot, m):
-    default = await db.get_configs("01")
-    temp.CONFIGS[m.from_user.id] = default
-    await db.update_configs(m.from_user.id, default)
-    await m.reply("Successfully Settings Reseted ✔️")
+   default = await db.get_configs("01")
+   temp.CONFIGS[m.from_user.id] = default
+   await db.update_configs(m.from_user.id, default)
+   await m.reply("successfully settings reseted ✔️")
 
-
-
-
-@Client.on_message(filters.command('resetall') & filters.user(Config.OWNER_ID))
+@Client.on_message(filters.command('resetall') & filters.user(Config.BOT_OWNER_ID))
 async def resetall(bot, message):
   users = await db.get_all_users()
-  sts = await message.reply("Processing")
-  TEXT = "Total: {}\nSuccess: {}\nFailed: {}\nExcept: {}"
+  sts = await message.reply("**processing**")
+  TEXT = "total: {}\nsuccess: {}\nfailed: {}\nexcept: {}"
   total = success = failed = already = 0
   ERRORS = []
   async for user in users:
@@ -192,19 +167,15 @@ async def resetall(bot, message):
          failed += 1
   if ERRORS:
      await message.reply(ERRORS[:100])
-  await sts.edit("Completed\n" + TEXT.format(total, success, failed, already))
+  await sts.edit("completed\n" + TEXT.format(total, success, failed, already))
   
-
-
 async def get_configs(user_id):
   #configs = temp.CONFIGS.get(user_id)
   #if not configs:
   configs = await db.get_configs(user_id)
   #temp.CONFIGS[user_id] = configs 
   return configs
-
-
-
+                          
 async def update_configs(user_id, key, value):
   current = await db.get_configs(user_id)
   if key in ['caption', 'duplicate', 'db_uri', 'forward_tag', 'protect', 'file_size', 'size_limit', 'extension', 'keywords', 'button']:
@@ -214,7 +185,6 @@ async def update_configs(user_id, key, value):
  # temp.CONFIGS[user_id] = value
   await db.update_configs(user_id, current)
     
-
 def parse_buttons(text, markup=True):
     buttons = []
     for match in BTN_URL_REGEX.finditer(text):
@@ -236,14 +206,3 @@ def parse_buttons(text, markup=True):
     if markup and buttons:
        buttons = InlineKeyboardMarkup(buttons)
     return buttons if buttons else None
-
-
-
-
-
-
-# Jishu Developer 
-# Don't Remove Credit 🥺
-# Telegram Channel @Madflix_Bots
-# Backup Channel @JishuBotz
-# Developer @JishuDeveloper
